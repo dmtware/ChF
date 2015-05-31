74389b8385223eff04ce38a714e23753f1df2cba
dmtware <info@dmtware.com>
2015-05-31 09:23:50 +0100

    Singleton - first attempt

--- app/src/main/java/com/dmtware/chf/MainActivity.java
+++ app/src/main/java/com/dmtware/chf/MainActivity.java
@@ -1,25 +1,26 @@
 package com.dmtware.chf;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.dmtware.chf.model.Token;
+import com.dmtware.chf.network.AppController;
 import com.google.gson.Gson;
-
+import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class MainActivity extends ActionBarActivity implements LoginFragment.LoginListener {
 
@@ -56,7 +57,7 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
     // this gets called by the Login Fragment when the usr clicks button
     @Override
     public void getLoginDetails(String userName, String password) {
-        getToken(userName, password);
+        getToken2(userName, password);
     }
 
     // Login button
@@ -102,5 +103,60 @@ public class MainActivity extends ActionBarActivity implements LoginFragment.Log
             }
         };
         queue.add(request);
+    }
+
+    // Login button
+    public void getToken2(String userName, String password){
+
+        // byte[] loginBytes = ("TomaszUser" + ":" + "Tomcat39").getBytes();
+
+        // generate basic authentication
+        byte[] loginBytes = (userName + ":" + password).getBytes();
+        final StringBuilder loginBuilder = new StringBuilder()
+                .append("Basic ")
+                .append(Base64.encodeToString(loginBytes, Base64.DEFAULT));
+
+        // POST request
+
+        // Tag used to cancel the request
+        String tag_json_obj = "json_obj_req";
+
+        // Progress dialog
+        final ProgressDialog pDialog = new ProgressDialog(this);
+        pDialog.setMessage("Loading...");
+        pDialog.show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, CF_URL, null , new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+
+                Token myToken = new Gson().fromJson(String.valueOf(response), Token.class);
+
+                TokenFragment tokenFragment = (TokenFragment)getSupportFragmentManager().findFragmentById(R.id.fragment);
+                tokenFragment.setTokenText("TOKEN: " + myToken.sessionToken);
+
+                currentToken = myToken.sessionToken;
+                pDialog.hide();
+
+            }
+            // if error
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError volleyError) {
+                pDialog.hide();
+                Toast.makeText(MainActivity.this, "The user name or password is incorrect", Toast.LENGTH_LONG).show();
+            }
+            // headers
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("CF-VersionInfo", "AppKey=C1A379CE-C0C0-43E9-91C7-9E80F5AC6452;AppVersion=1.0.0;OS=Windows;Platform=DellUA=Stuff");
+                params.put("Authorization", loginBuilder.toString());
+                return params;
+            }
+        };
+        // add to the queue
+        AppController.getInstance().addToRequestQueue(request, tag_json_obj);
     }
 }
